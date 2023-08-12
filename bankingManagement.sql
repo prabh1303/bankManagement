f61e719833f46f247f54f9cc87958aafe850ad09
Prabhjot Singh Bhatia <72307426+prabh1303@users.noreply.github.com>
2023-08-12 19:39:12 +0530

    added sql commands

---
+++ bankingManagement.sql
@@ -0,0 +1,206 @@
+--customer table
+create table Customer (
+    customer_id INT,
+    home_branch VARCHAR(50),
+    name VARCHAR(50),
+    email VARCHAR(50),
+    DOB DATE,
+    house_address VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    balance INT,
+    acc_no INT,
+    PRIMARY KEY(customer_id)
+);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (131, 'Huayllati', 'Evey Rose', 'erose0@spotify.com', '06-FEBRUARY-1994', '90517', 'Bonner Lane', 'California', 2846068,1000);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (496, 'San Carlos', 'Mercie Dackombe', 'mdackombe1@army.mil', '29-APRIL-1992', '2', 'Barnett Trail', 'California', 211334,2455);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (555, 'Zaolin', 'Giulio Miskimmon', 'gmiskimmon2@drupal.org', '20-MAY-1992', '700', 'Esker Court', 'California', 1885991,7662);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (237, 'Piraquara', 'Ingram Keeting', 'ikeeting3@printfriendly.com', '27-FEBRUARY-1987', '39', 'Vidon Way', 'California', 3236662,9827);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (478, 'Áyios Yeóryios', 'Emmie Gartrell', 'egartrell4@rediff.com', '04-MARCH-1991', '47', 'Washington Alley', 'California', 1069704,3231);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (482, 'Konggar', 'Estevan Shimon', 'eshimon5@alexa.com', '21-APRIL-1989', '41024' , 'Fairview Avenue', 'California', 4217430,9888);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (123, 'Beijiang', 'Jay Ryles', 'jryles6@blog.com', '27-MARCH-1993', '71378','Express Lane', 'California', 3726391,1300);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (972, 'Obubra', 'Flory Clooney', 'fclooney7@pbs.org', '15-JULY-1996', '73', 'Petterle Way', 'California', 4917672,2311);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (312, 'Féres', 'Charlot Strank', 'cstrank8@uol.com.br', '10-APRIL-1996', '6933', 'Donald Street', 'California', 204066,1001);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (579, 'Sokyryany', 'Bobine Emslie', 'bemslie9@acquirethisname.com', '06-FEBRUARY-1996', '2', 'Summer Ridge Crossing', 'California', 4288422,2457);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (53, 'Nanxindian', 'Pansie Stoneley', 'pstoneleya@constantcontact.com', '18-MAY-1989', '60209', 'Sullivan Avenue', 'California', 4151444,7663);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (759, 'Pinamalayan', 'Donni McSweeney', 'dmcsweeneyb@sciencedirect.com', '28-APRIL-1988', '92', 'Paget Point', 'California', 3032477,9829);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (427, 'Yevlakh', 'Micah Tomkins', 'mtomkinsc@taobao.com', '31-DECEMBER-1988', '6', 'Crownhardt Drive', 'California', 2885458,3232);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (968, 'Tbng Manchey', 'Bernardina Blankau', 'bblankaud@drupal.org', '27-FEBRUARY-1992', '3', 'Golden Leaf Junction', 'California', 2576207,1002);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (712, 'Firmat', 'Pennie Sliman', 'pslimane@harvard.edu', '09-APRIL-1989', '2248', 'Mesta Street', 'California', 1956571,1003);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (60, 'Abut', 'Herve Slimings', 'hslimingsf@ask.com', '29-MAY-1995', '7857', 'Havey Lane', 'California', 4285790,1004);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (96, 'Cijeungjing Kaler', 'Dolph Curteis', 'dcurteisg@nymag.com', '15-MARCH-1988', '5730', 'Gateway Place', 'California', 1999536,1005);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (108, 'Evansville', 'Caitlin Lux', 'cluxh@sciencedaily.com', '24-APRIL-1989', '44', 'Kenwood Park', 'California', 3394978,1006);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (266, 'Sinarbakti', 'Reinaldo Putland', 'rputlandi@irs.gov', '07-JULY-1991', '8', 'Sheridan Parkway', 'California', 1985368,1007);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (458, 'Sopo', 'Inessa Ginglell', 'iginglellj@wordpress.com', '04-MAY-1990', '51', 'Lakeland Road', 'California', 1536181,1008);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (844, 'Jawhar', 'Pablo Francescuccio', 'pfrancescucciok@cpanel.net', '14-FEBRUARY-1993', '5', 'Blaine Park', 'California', 1538025,1009);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (852, 'Kozloduy', 'Myrwyn Diegan', 'mdieganl@dailymotion.com', '27-MAY-1990', '65', 'Bay Hill', 'California', 1261715,2000);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (753, 'Svyetlahorsk', 'Lenka Durbridge', 'ldurbridgem@dmoz.org', '22-APRIL-1991', '669', 'Mayfield Point', 'California', 1184117,2001);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (946, 'Stopnica', 'Lance Shasnan', 'lshasnann@g.co', '23-MARCH-1990', '060', 'Maple Circle', 'California', 3168015,2002);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (343, 'Ocoruro', 'Hamilton Chatell', 'hchatello@netvibes.com', '05-FEBRUARY-1989', '84', 'Sachtjen Point', 'California', 1152027,2005);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (92, 'Hanyuan', 'Mikkel Reef', 'mreefp@w3.org', '30-MAY-1995', '81', 'Jay Point', 'California', 1540937,2006);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (785, 'Serzedo', 'Vivien Brass', 'vbrassr@squidoo.com', '20-APRIL-1988', '3453', 'Lunder Junction', 'California', 3914293,2007);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (250, 'Ujazd', 'Dalt Wakeling', 'dwakelings@mtv.com', '22-MAY-1989', '87', 'Miller Circle', 'California', 2757376,2008);
+insert into Customer (customer_id, home_branch, name, email, DOB, house_address, city, state, balance,acc_no) values (52, 'Barrie', 'Winonah Ship', 'wshipt@google.co.uk', '13-FEBRUARY-1992', '26499', 'Norway Maple Drive', 'California', 497033,2009);
+
+--Complaint Table
+create table Complaint(
+    Status VARCHAR(50),
+    customer_id INT,
+    type VARCHAR(100),
+    no_of_days INT,
+    foreign key(customer_id) references Customer(customer_id),
+    primary key(customer_id,type)
+);
+
+insert into Complaint (Status,customer_id,type,no_of_days) values ('false',785,'debit card',3);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',52,'transaction issues',7);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('false',108,'credit card',14);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('false',266,'debit card',16);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('false',946,'behaviour issues with the staff',1);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',60,'transaction issues',12);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',250,'debit card',20);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('false',427,'credit card',2);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',968,'behaviour issues with the staff',5);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',712,'debit card',11);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',96,'transaction issues',13);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',844,'credit card',5);
+insert into Complaint (Status,customer_id,type,no_of_days) values ('true',343,'transaction issues',7);
+
+--LOAN Table
+create table LOAN (
+    Loan_number INT,
+    Loan_date DATE,
+    Loan_amount INT,
+    customer_id INT,
+    foreign key(customer_id) references Customer(customer_id),
+    primary key(customer_id,Loan_number)
+);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (52, '29-MAY-2012', 499995,131);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (60, '21-APRIL-2016', 381695,496);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (96, '04-JULY-2019', 398073,555);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (123, '15-MAY-2013', 101992,237);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (131, '25-APRIL-2017', 12929,478);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (250, '18-DECEMBER-2015', 395558,482);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (266, '09-MAY-2018', 133980,123);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (312, '14-APRIL-2019', 69153,972);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (482, '10-DECEMBER-2018', 269001,312);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (496, '02-MAY-2017', 65446,579);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (753, '06-JULY-2012', 223039,53);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (946, '26-JULY-2010', 350521,759);
+insert into LOAN (Loan_number, Loan_date, Loan_amount,customer_id) values (972, '10-MAY-2017', 470133,427);
+
+create table manager (
+    name varchar(50),
+    manager_id INT,
+    phone_no INT,
+    customer_id INT,
+    foreign key(customer_id) references Customer(customer_id),
+    primary key(customer_id)
+);
+
+--CUSTOMER CANNOT OPEN A BANK ACCOUNT ON SUNDAY
+CREATE OR REPLACE TRIGGER Customer_SUNDAY
+BEFORE INSERT OR UPDATE OR DELETE ON CUSTOMER
+BEGIN 
+    IF RTRIM(UPPER(TO_CHAR(SYSDATE,'DAY')))='SUNDAY' THEN
+        RAISE_APPLICATION_ERROR(-20022,'NO OPERARTION CAN BE PERFORMED ON SUNDAY');
+    END IF;
+END;
+
+--LOAN CANNOT BE GIVEN ON SUNDAY
+CREATE OR REPLACE TRIGGER LOAN_SUNDAY
+BEFORE INSERT OR UPDATE OR DELETE ON LOAN
+BEGIN 
+    IF RTRIM(UPPER(TO_CHAR(SYSDATE,'DAY')))='SUNDAY' THEN
+        RAISE_APPLICATION_ERROR(-20022,'NO OPERARTION CAN BE PERFORMED ON SUNDAY');
+    END IF;
+END;
+
+--COMPLAINT CANNOT BE LODGED ON SUNDAY
+CREATE OR REPLACE TRIGGER Complaint_SUNDAY
+BEFORE INSERT OR UPDATE OR DELETE ON Complaint
+BEGIN 
+    IF RTRIM(UPPER(TO_CHAR(SYSDATE,'DAY')))='SUNDAY' THEN
+        RAISE_APPLICATION_ERROR(-20022,'NO OPERARTION CAN BE PERFORMED ON SUNDAY');
+    END IF;
+END;
+
+--IF NEW BALANCE IS LESS THAN 0, IT WILL RAISE A WARNING
+CREATE OR REPLACE TRIGGER UPDATE_balance
+BEFORE UPDATE ON Customer
+FOR EACH ROW
+BEGIN
+    IF :NEW.balance<0 THEN
+        RAISE_APPLICATION_ERROR(-20000,'NEW BALANCE CANNOT BE LESS THAN 0');
+    END IF;
+END;
+
+--IF LOAN AMOUNT BECOMES 0, LOAN RECORD IS DELETED
+CREATE OR REPLACE TRIGGER LOAN_AMT
+AFTER UPDATE ON LOAN
+FOR EACH ROW
+BEGIN
+    IF :NEW.loan_amount= 0 THEN
+        DELETE FROM LOAN WHERE Loan_amount = 0;
+    END IF;
+END;
+
+--REDIRECTING COMPLAINT TO MANAGER IF 15 DAYS HAS BEEN PASSED
+DECLARE 
+CURSOR C1 IS SELECT  * FROM COMPLAINT;
+REC C1%ROWTYPE;
+BEGIN
+OPEN C1;
+LOOP
+    FETCH C1 INTO REC;
+    EXIT WHEN C1%NOTFOUND;
+    IF REC.no_of_days > 15 and status LIKE ‘true’ THEN
+        INSERT INTO MANAGER(NAME, MANAGER_ID, PHONE_NO, CUSTOMER_ID) VALUES('Nikhil', 555, 7837986701, REC.Customer_ID);
+    END IF;
+END LOOP;
+CLOSE C1;
+END;
+
+--TO SEE PENDING COMPLAINTS OF CUSTOMERS WITH NO OF DAYS>15
+SELECT * FROM MANAGER;
+
+--TOTAL LOAN GIVEN BY BANK
+CREATE OR REPLACE FUNCTION TOTAL_LOAN RETURN NUMBER IS  
+s NUMBER;
+BEGIN
+Select sum(LOAN_AMOUNT) into s from LOAN;
+RETURN(s);
+END;
+
+DECLARE 
+C NUMBER
+BEGIN
+C:=TOTAL_LOAN();
+DBMS_OUTPUT.PUT_LINE('TOTAL LOAN '||C);
+END;
+
+--CALCULATES TOTAL LOAN TAKEN BY A CUSTOMER
+DECLARE 
+TOTAL NUMBER;
+PROCEDURE TOTAL_LOAN_CUST( CUSTID IN NUMBER) IS
+BEGIN
+SELECT SUM(*) INTO TOTAL FROM LOAN;
+GROUP BY LOAN.CUSTOMER_ID;
+WHERE CUSTID=LOAN.CUSTOMER_ID;
+BEGIN
+TOTAL_LOAN_CUST(131);
+DBMS_OUTPUT.PUT_LINE('TOTAL LOAN AMOUNT IS '||TOTAL);
+END;
+
+--CHECKING WHETHER A PARTICULAR CUSTOMER HAS TAKEN LOAN OR NOT
+DECLARE
+CHECK_LOAN EXCEPTION;
+CHECK NUMBER = &CHECK;
+BEGIN
+IF NOT EXISTS ( SELECT * FROM LOAN WHERE CUSTOMER_ID = 92) THEN
+RAISE CHECK_LOAN;
+END IF;
+EXCEPTION
+WHEN CHECK_LOAN THEN 
+DBMS_OUTPUT.PUT_LINE('NO LOAN RECORD FOUND');
+END;
